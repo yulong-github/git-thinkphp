7ad6da301ed8721b4c1b68d091f1abaf82d8f074
yulong <1358002318@qq.com>
2019-11-29 14:37:56 +0800

    修改根命名空间为app，更容易理解

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ namespace app\admin\controller;
+ 
+ 
+ class Index
+ {
+     public function index() {
+         return "this is admin index index";
+     }   
+ }
+
